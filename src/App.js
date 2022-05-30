6fd4894f96792701f78bbf9951513c35de5824c8
satsukiya <kai.walker.0329@gmail.com>
2022-05-30 18:23:18 +0900

    不要なimport文を削除

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-//import Welcome from './sample';
-import { Welcome, MorningCall, Profile } from './sample';
 import React from 'react'
 import { Header } from './components/Header';
 import { Skills } from './components/Skills';
