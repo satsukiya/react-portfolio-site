9d92e2773ead1c5c30f7de016e1b0d08cd7c89f1
satsukiya <kai.walker.0329@gmail.com>
2022-06-04 20:29:35 +0900

    5-5 完了

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,18 +1,56 @@
-import { useEffect } from 'react';
+import Circle from 'react-circle';
+import { useEffect, useReducer } from 'react';
 import axios from 'axios';
+import { skillReducer, initialState, actionTypes } from '../reducers/skillReducer';
+import { requestStates } from '../constants';
 
 export const Skills = () => {
 
-  //useEffect(() => { axios.get('URL') }, []);
+  const [state, dispatch] = useReducer(skillReducer, initialState);
 
   useEffect(() => {
+
     let url = "https://api.github.com/users/satsukiya/repos";
+    /*
     axios.get(url)
       .then((response) => {
-        console.log(response);
+        const languageList = response.data.map(res => res.language);
+        const countedLanguageList = generateLanguageCountObj(languageList);
+        setLanguageList(countedLanguageList);
       });
+    */
+    dispatch({ type: actionTypes.fetch });
+    axios.get(url)
+      .then((response) => {
+        const languageList = response.data.map(res => res.language);
+        const countedLanguageList = generateLanguageCountObj(languageList);
+        dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } });
+      })
+      .catch(() => {
+        dispatch({ type: actionTypes.error });
+      });
+
   }, []);
 
+  const generateLanguageCountObj = (allLanguageList) => {
+    const notNullLanguageList = allLanguageList.filter(language => language != null);
+    const uniqueLanguageList = [...new Set(notNullLanguageList)];
+    return uniqueLanguageList.map(item => {
+      return {
+        language: item,
+        count: allLanguageList.filter(language => language === item).length
+      }
+    });
+  }
+
+  const converseCountToPercentage = (count) => {
+    if (count > 10) { return 100; }
+    return count * 10;
+  };
+
+  const sortedLanguageList = () => (
+    state.languageList.sort((firstLang, nextLang) => firstLang.count - nextLang.count)
+  )
 
   return (
     <div id="skills">
@@ -21,6 +59,34 @@ export const Skills = () => {
           <h2>Skills</h2>
         </div>
         <div className="skills-container">
+          {
+            state.requestState === requestStates.loading && (
+              <p className="description">
+                取得中...
+              </p>
+            )
+          }
+          {
+            state.requestState === requestStates.success && (
+              sortedLanguageList().map((item, index) => (
+                <div className="skill-item" key={index}>
+                  <p className="description"><strong>{item.language}</strong></p>
+                  <Circle
+                    animate
+                    progress={converseCountToPercentage(item.count)}
+                  />
+                </div>
+              )
+              )
+            )
+          }
+          {
+            state.requestState === requestStates.error && (
+              <p className="description">
+                エラーが発生しました
+              </p>
+            )
+          }
         </div>
       </div>
     </div>
